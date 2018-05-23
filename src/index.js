c94946037b2a03d8cb155aef23820e1a13b287ed
Pascal Prins <pascal.prins@foobar-it.com>
2018-05-23 13:38:52 +0200

    Minor bugfix + package rename

--- src/index.js
+++ src/index.js
@@ -8,23 +8,26 @@ class ServerlessPlugin {
   constructor(serverless, options) {
     this.serverless = serverless;
     this.options = options;
-    this.log = serverless.cli.consoleLog;
 
     this.hooks = {
       'before:aws:package:finalize:mergeCustomProviderResources': this.fixApiGatewayDeployment.bind(this),
     };
   }
 
+
   fixApiGatewayDeployment() {
     const template = this.serverless.service.provider.compiledCloudFormationTemplate;
+    const log = this.serverless.cli.consoleLog;
 
     // Find the deployment resource in CloudFormation template
     const gatewayDeployResources = Object.keys(template.Resources).filter(id => template.Resources[id].Type === 'AWS::ApiGateway::Deployment');
 
+    gatewayDeployResources.forEach(addTimestampResourceId);
+
     function addTimestampResourceId(resourceId) {
       // Check if `deploymentId` already ends with digits
       if (!/\d+$/.test(resourceId)) {
-        this.log(`Appending timestamp to '${resourceId}' to force new deployment!`);
+        log(`Appending timestamp to '${resourceId}' to force new deployment!`);
         // Add new resource (with timestamp)
         const newResourceId = resourceId + Date.now();
         template.Resources[newResourceId] = template.Resources[resourceId];
@@ -32,8 +35,6 @@ class ServerlessPlugin {
         delete template.Resources[resourceId];
       }
     }
-
-    gatewayDeployResources.forEach(addTimestampResourceId);
   }
 }
 
